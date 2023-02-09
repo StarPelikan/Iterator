ec6bd37b3cc1a3712b6c7bcc9be776ff93c714eb
StarPelikan <cccp-89@bk.ru>
2023-02-09 14:45:13 +0300

    Initial commit

---
+++ src/main/java/org/example/Randoms.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    private int min;
+    private int max;
+
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new MySuperPuperIterator();
+    }
+
+    public class MySuperPuperIterator implements Iterator {
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public Integer next() {
+            return new Random().nextInt(max - min + 1) + min;
+        }
+    }
+}
+
